d09d0eff8917b111b93a2fb066201d4b8a3af41e
vishu pratap <vishurizz01@gmail.com>
2024-06-25 15:34:31 +0530

    leetcode

--- src/Leetcode/Leetcode287.java
+++ src/Leetcode/Leetcode287.java
@@ -2,7 +2,34 @@ package Leetcode;
 
 public class Leetcode287 {
     public static void main(String[] args) {
+        int[] array = {3,3,3,1,2,3};
+        System.out.println(findDuplicate(array));
 
     }
+    public static int findDuplicate(int[] nums){
+        int i = 0;
 
+        while(i < nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            } else{
+                i++;
+            }
+        }
+        return duplicateChecker(nums);
+    }
+    public static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    public static int duplicateChecker(int[] arr){
+        int lastIndex = arr.length-1;
+        if(arr[lastIndex] - 1  != arr[lastIndex-1]){
+            return arr[lastIndex];
+        }
+        return -1;
+    }
 }
+
