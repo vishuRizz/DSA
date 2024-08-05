f3b2654c2edb5f12396905daee1054b8d7e7071f
vishu pratap <vishurizz01@gmail.com>
2024-08-05 14:20:59 +0530

    newQuestions

--- src/recursion_problems/QuickSortByRecursion.java
+++ src/recursion_problems/QuickSortByRecursion.java
@@ -5,33 +5,40 @@ import java.util.Arrays;
 public class QuickSortByRecursion {
     public static void main(String[] args) {
      int[] array = {5, 4, 3, 2, 1};
-     quickSort(array, 0, array.length -1);
+     sort(array, 0, array.length -1);
         System.out.println(Arrays.toString(array));
     }
-    static void quickSort(int[] arr, int low, int high){
-       if(low<high){
-           return;
-       }
-        int start = low;
-        int end = high;
-        int mid = start + (end-start)/2;
-        int pivot = arr[mid];
-        while (start <= end) {
-            while (arr[start] < pivot) {
-                    start++;
-                }
-            while (pivot < arr[end]) {
-                end--;
+    static void sort(int[] nums, int low, int hi) {
+        if (low >= hi) {
+            return;
+        }
+
+        int s = low;
+        int e = hi;
+        int m = s + (e - s) / 2;
+        int pivot = nums[m];
+
+        while (s <= e) {
+
+            // also a reason why if its already sorted it will not swap
+            while (nums[s] < pivot) {
+                s++;
             }
-            if(start <= end){
-                int temp = arr[start];
-                arr[start] = arr[end];
-                arr[end] = temp;
-                start++;
-                end--;
+            while (nums[e] > pivot) {
+                e--;
+            }
+
+            if (s <= e) {
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
             }
         }
-        quickSort(arr, low, end);
-        quickSort(arr, start, high);
+
+        // now my pivot is at correct index, please sort two halves now
+        sort(nums, low, e);
+        sort(nums, s, hi);
     }
 }
