2d845793053e919c6a48ebf1f065559effa8759b
vishuRizz <vishurizz0@gmail.com>
2024-12-30 17:31:01 +0530

    adding cpp

---
+++ output/haha.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){  
+    int age;
+    cin >> age;
+    if (age < 18){
+        cout << "child";
+    } else{
+        cout << "adult";
+    }
+}
+
+
